3662a57bc6a396b0f35be4be15c7765b234daead
Tamás Matyó <tomi.matyo@gmail.com>
2026-02-08 18:41:28 +0100

    chore: seo description and gtag added

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -11,12 +11,17 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
+import { createGtag } from 'vue-gtag';
 import VueApexCharts from 'vue3-apexcharts';
 import SidebarProvider from './components/layout/SidebarProvider.vue';
 import ThemeProvider from './components/layout/ThemeProvider.vue';
 import { t } from './helpers/i18n';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appGtag = import.meta.env.VITE_GTAG || '';
+const gtag = createGtag({
+    tagId: appGtag,
+});
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
@@ -42,6 +47,7 @@ createInertiaApp({
         app.use(plugin);
         app.use({ install: (app) => (app.config.globalProperties.$t = t) });
         app.use(VueApexCharts);
+        app.use(gtag);
         app.mount(el);
     },
     progress: {
